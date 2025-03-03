e738fd9c7681cda1d9c90b2a77120e56979017fb
Aakash <aakashg768@outlook.com>
2025-03-03 16:46:53 +0530

    server edited

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 
-const corsOptions = {
-    origin: process.env.FRONTEND_URL,
-    optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
-  };
+// const corsOptions = {
+//     origin: process.env.FRONTEND_URL,
+//     optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+//   };
   
-  app.use(cors(corsOptions));
+//   app.use(cors(corsOptions));
 
 app.use(express.json());
 
